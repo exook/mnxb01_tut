e723f74b2ca3d35af52cc5b30dd5e6442a07a7c9
Alexander Ekman <alexander.ekman95@gmail.com>
2017-10-24 13:45:29 +0200

    Started work on scientists excercise

---
+++ 5a/scientists.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+	vector<string> list;
+	for(size_t i = 0; i < list.size(); ++i){ cout<<list.at(i)<<endl;}
+	list.push_back("Lovelace");
+	list.push_back("Meitner");
+	list.push_back("Wu");
+	list.push_back("Lamarr");
+	for(size_t i = 0; i < list.size(); ++i){ cout<<list.at(i)<<endl;}
+
+	return 0;
+}
